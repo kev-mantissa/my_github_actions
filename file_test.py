47a6b89f020b68fb91ed35de14884d68e3b3273c
Ubuntu <ubuntu@ip-172-31-42-157.eu-west-1.compute.internal>
2023-07-22 12:06:35 +0000

    Ajout du fichier file_test

---
+++ file_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+def test_calc_addition():
+    output=2+4
+    assert output == 6
+
+def test_calc_substraction():
+    output =2-4
+    assert ouput ==-2
+
+def test_calc_multiply():
+    output=2*4
+    assert output == 8
+
+def test_coucou():
+    output='hello'
+    assert output == 'hello'
+
