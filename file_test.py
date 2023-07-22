8a7c818b395e75f9c2664b734da615e573306fda
Ubuntu <ubuntu@ip-172-31-42-157.eu-west-1.compute.internal>
2023-07-22 12:21:44 +0000

    Refactoring de file_test

--- file_test.py
+++ file_test.py
@@ -1,18 +1,18 @@
-import pytest
-
 def test_calc_addition():
-    output=2+4
+    output = 2+4
     assert output == 6
 
+
 def test_calc_substraction():
-    output =2-4
-    assert output ==-2
+    output = 2-4
+    assert output == -2
+
 
 def test_calc_multiply():
-    output=2*4
+    output = 2*4
     assert output == 8
 
-def test_coucou():
-    output='hello'
-    assert output == 'hello'
 
+def test_coucou():
+    output = 'hello'
+    assert output == 'hello'
